7b64694e9ac9204269c24ec92981a981af066fc8
Souskiv <108626982+Souskiv@users.noreply.github.com>
2025-02-27 15:51:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let ingresarAmig = document.getElementById("amigo");
+let amigos = [];
+let lista = document.getElementById("listaAmigos");
+let result = document.getElementById("resultado");
+
+function agregarAmigo(){
+    if (ingresarAmig == "")
+        alert("Por favor, inserte un nombre.")
+    else {
+        amigos.push(ingresarAmig.value);
+        lista.innerHTML += `<li>${ingresarAmig.value}</li>`;
+    }
+    limpiarCaja();
+}
+function limpiarCaja(){
+    document.querySelector("#amigo").value = "";
+}
+function sortearAmigo() {
+    let aleatorio = Math.floor(Math.random() * amigos.length);
+    let secreto = amigos[aleatorio];
+    result.innerHTML = `<li>Tu amigo secreto es: ${secreto}.</li>`;
+}
